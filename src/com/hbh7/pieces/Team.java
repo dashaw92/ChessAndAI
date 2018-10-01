1f4a4a035a776901aabda3f2e5b189cf4526723c
Daniel Shaw <dashaw92@users.noreply.github.com>
2018-10-01 14:43:22 -0400

    Correct one typo in Team#getNameProper

--- src/com/hbh7/pieces/Team.java
+++ src/com/hbh7/pieces/Team.java
@@ -7,6 +7,6 @@ public enum Team {
 	 * @return The name of the team properly formatted: BLACK -> Black
 	 */
 	public String getNameProper() {
-		return name().substring(0).toUpperCase() + name().substring(1).toLowerCase();
+		return name().substring(0, 1).toUpperCase() + name().substring(1).toLowerCase();
 	}
 }
